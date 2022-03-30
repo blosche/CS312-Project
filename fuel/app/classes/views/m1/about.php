0379d4c70b88d5321644d167e1bffba69d3440b3
Boone Losche <blosche@gmail.com>
2022-03-30 13:15:56 -0600

    Update about.php

--- fuel/app/classes/views/m1/about.php
+++ fuel/app/classes/views/m1/about.php
@@ -2,20 +2,28 @@
             <p>We are a team of qualified individuals who can fix your pc which has a virus. Let us fix your computer.</p>
             <div id="aboutPics">
                 <div>
+                <h2>Mike</h2>
                  <?php echo Asset::img('Mike\'s-Face.png'); ?>
+                 <p>I am Mike, I can help you safe guard your Amazon gift card codes. I have a good degree in computer stuff. Go our local sport team!</p>
 
-                        <h2>Mike</h2>
+                     
                 </div>
                 <div>
+                <h2>Thomas</h2>
                 <?php echo Asset::img('Thomas\'s-Face.png'); ?>
-                        <h2>Thomas</h2>
+                        <p>My name is Thomas and I am the best in the world at securing your passwords and storing them for you. I also enjoy coffee and other things you like.</p>
+                       
                 </div>
                 <div>
-                    <img src="./media/placeholder.png" width="250px">
-                        <h2>Braeden</h2>
+                <h2>Braeden</h2>
+                <?php echo Asset::img('Braedon\'s-Face.jpeg'); ?>
+                       
+                        <p>My name is Braeden Smith, I have a degree from a university that qualifies me as a support technician for your home or office computer. My skills including encrypting important files, scam prevention, and viruses.</p>
                 </div>
                 <div>
+                <h2>Boone</h2>
                 <?php echo Asset::img('Boone\'s-Face.jpg'); ?>
-                        <h2>Boone</h2>
+                        <p>Hello there fellow computer person, my name is Boone Losche and I specalize in banking protection and nigerian princes.</p>
+                        
                 </div>
             </div>
