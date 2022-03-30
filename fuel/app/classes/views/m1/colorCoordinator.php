941e58afc0dddf5f9d51c6894b50bac0b8cc60b5
Boone Losche <blosche@gmail.com>
2022-03-30 15:59:57 -0600

    change color cordinator

--- fuel/app/classes/views/m1/colorCoordinator.php
+++ fuel/app/classes/views/m1/colorCoordinator.php
@@ -1,1 +1,18 @@
-<p>HOLD</p>
+<?php
+echo Form::open(array(
+  "action" => 'index/' . Input::uri(),
+  "method" => "get",
+  "id" => "color_form"
+));
+echo '<div>';
+echo Form::label('Number of Rows/Columns (1-26): &nbsp', 'table');
+echo Form::input('table', '');
+echo '</div>';
+echo '<br><br>';
+echo Form::label('Number of Colors (1-10): &nbsp &nbsp &nbsp &nbsp', 'colors');
+echo Form::input('colors', '');
+echo '<br><br>';
+echo Form::submit('submitBtn', 'submit');
+  
+echo Form::close();
+?>
